7d753f5cf846e31ca93b435e115b0ecf1307ad2e
ximelocru11 <ax.lopez@uniandes.edu.co>
2025-09-21 23:28:22 -0500

    final final

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 // src/app/page.tsx
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold">Mi Primera App con Next.js</h1>
+      <h1 className="text-4xl font-bold">Bookstore</h1>
+      <p className="mt-4 text-lg">Bienvenido a la aplicación de Bookstore!</p>
+      <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-12">  
+      <Link href="/authors" className="px-3 py-1 rounded-lg shadow bg-[var(--color-boton)] text-white hover:opacity-80">
+      Autores
+      </Link>
+      <Link href="/create" className="px-3 py-1 rounded-lg shadow bg-[var(--color-boton)] text-white hover:opacity-80">
+      Crear autor
+      </Link>
+      </div>
     </main>
   );
 }
